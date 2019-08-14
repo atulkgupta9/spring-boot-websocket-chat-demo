6ab280607605d4f724374690943ec54fd94cde32
atul <atul080796@gmail.com>
2019-08-14 23:24:01 +0530

    commit 1

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,121 +1,135 @@
 'use strict';
+$(document).ready(function () {
 
-var usernamePage = document.querySelector('#username-page');
-var chatPage = document.querySelector('#chat-page');
-var usernameForm = document.querySelector('#usernameForm');
-var messageForm = document.querySelector('#messageForm');
-var messageInput = document.querySelector('#message');
-var messageArea = document.querySelector('#messageArea');
-var connectingElement = document.querySelector('.connecting');
+    var c = [];
+    var d = [];
+    var e = [];
 
-var stompClient = null;
-var username = null;
-
-var colors = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-];
-
-function connect(event) {
-    username = document.querySelector('#name').value.trim();
-
-    if(username) {
-        usernamePage.classList.add('hidden');
-        chatPage.classList.remove('hidden');
-
-        var socket = new SockJS('/ws');
-        stompClient = Stomp.over(socket);
-
-        stompClient.connect({}, onConnected, onError);
-    }
-    event.preventDefault();
-}
-
-
-function onConnected() {
-    // Subscribe to the Public Topic
-    stompClient.subscribe('/topic/public', onMessageReceived);
-
-    // Tell your username to the server
-    stompClient.send("/app/chat.addUser",
-        {},
-        JSON.stringify({sender: username, type: 'JOIN'})
-    )
-
-    connectingElement.classList.add('hidden');
-}
-
-
-function onError(error) {
-    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
-    connectingElement.style.color = 'red';
-}
-
-
-function sendMessage(event) {
-    var messageContent = messageInput.value.trim();
-
-    if(messageContent && stompClient) {
-        var chatMessage = {
-            sender: username,
-            content: messageInput.value,
-            type: 'CHAT'
+    function plotmylife(alpha,beta,gamma){
+        var set2 = {};
+        c.push(alpha);
+        d.push(beta);
+        e.push(gamma);
+        set2['x'] = c;
+        set2['y'] = d;
+        set2['z'] = e;
+        console.log(c,d,e);
+        set2['mode'] = 'lines+markers';
+        set2['marker'] = {
+            color: 'rgb(255, 0, 0)',
+            size: 8
         };
-
-        stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
-        messageInput.value = '';
+        set2['name'] = "Convex Polygon that enclosed these points";
+        set2['line'] = {
+            color: 'rgb(128, 0, 128)',
+            width: 2
+        };
+        set2['type'] = 'scatter3d';
+        var pos = [set2];
+        var layout = {
+            title: 'Points convered by device'
+        };
+        Plotly.newPlot('myDiv', pos, layout, {showSendToCloud: false})
     }
-    event.preventDefault();
-}
+    plotmylife(0,0,0);
 
+    // original content
+    var usernamePage = document.querySelector('#username-page');
+    var chatPage = document.querySelector('#chat-page');
+    var usernameForm = document.querySelector('#usernameForm');
+    var messageForm = document.querySelector('#messageForm');
+    var messageInput = document.querySelector('#message');
+    var messageArea = document.querySelector('#messageArea');
+    var connectingElement = document.querySelector('.connecting');
 
-function onMessageReceived(payload) {
-    var message = JSON.parse(payload.body);
+    var stompClient = null;
+    var username = null;
 
-    var messageElement = document.createElement('li');
+    var colors = [
+        '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+        '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+    ];
 
-    if(message.type === 'JOIN') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' joined!';
-    } else if (message.type === 'LEAVE') {
-        messageElement.classList.add('event-message');
-        message.content = message.sender + ' left!';
-    } else {
-        messageElement.classList.add('chat-message');
+    function connect(event) {
+        username = document.querySelector('#name').value.trim();
 
-        var avatarElement = document.createElement('i');
-        var avatarText = document.createTextNode(message.sender[0]);
-        avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+        if (username) {
+            usernamePage.classList.add('hidden');
+            chatPage.classList.remove('hidden');
 
-        messageElement.appendChild(avatarElement);
+            var socket = new SockJS('/ws');
+            stompClient = Stomp.over(socket);
 
-        var usernameElement = document.createElement('span');
-        var usernameText = document.createTextNode(message.sender);
-        usernameElement.appendChild(usernameText);
-        messageElement.appendChild(usernameElement);
-    }
-
-    var textElement = document.createElement('p');
-    var messageText = document.createTextNode(message.content);
-    textElement.appendChild(messageText);
-
-    messageElement.appendChild(textElement);
-
-    messageArea.appendChild(messageElement);
-    messageArea.scrollTop = messageArea.scrollHeight;
-}
-
-
-function getAvatarColor(messageSender) {
-    var hash = 0;
-    for (var i = 0; i < messageSender.length; i++) {
-        hash = 31 * hash + messageSender.charCodeAt(i);
+            stompClient.connect({}, onConnected, onError);
+        }
+        event.preventDefault();
     }
 
-    var index = Math.abs(hash % colors.length);
-    return colors[index];
-}
 
-usernameForm.addEventListener('submit', connect, true)
-messageForm.addEventListener('submit', sendMessage, true)
+    function onConnected() {
+        // Subscribe to the Public Topic
+        stompClient.subscribe('/topic/public', onMessageReceived);
+
+        // Tell your username to the server
+        stompClient.send("/app/chat.addUser",
+            {},
+            JSON.stringify({sender: username, type: 'JOIN'})
+        )
+
+        connectingElement.classList.add('hidden');
+    }
+
+
+    function onError(error) {
+        connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+        connectingElement.style.color = 'red';
+    }
+
+
+    function sendMessage(event) {
+        var messageContent = messageInput.value.trim();
+
+        if (messageContent && stompClient) {
+            var chatMessage = {
+                sender: username,
+                content: messageInput.value,
+                type: 'CHAT'
+            };
+
+            stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage));
+            messageInput.value = '';
+        }
+        event.preventDefault();
+    }
+
+
+    function onMessageReceived(payload) {
+        var message = JSON.parse(payload.body);
+        console.log(message);
+        let arr = message.content.split(",");
+        let alpha = arr[0];
+        let beta = arr[1];
+        let gamma = arr[2];
+        console.log("alpha ", alpha, " beta ", beta, "gamma", gamma);
+        plotmylife(alpha,beta,gamma);
+    }
+
+
+    function getAvatarColor(messageSender) {
+        var hash = 0;
+        for (var i = 0; i < messageSender.length; i++) {
+            hash = 31 * hash + messageSender.charCodeAt(i);
+        }
+
+        var index = Math.abs(hash % colors.length);
+        return colors[index];
+    }
+
+    usernameForm.addEventListener('submit', connect, true)
+    messageForm.addEventListener('submit', sendMessage, true)
+
+
+
+
+
+})
--- src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
+++ src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
@@ -1,16 +1,36 @@
 package com.example.websocketdemo;
 
+import jssc.SerialPortList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.usb.*;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebsocketDemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws UsbException {
+		UsbServices services = UsbHostManager.getUsbServices( );
+		UsbHub	 root = services.getRootUsbHub( );
+		listDevices(root);
+	}
+	public static void listDevices(UsbHub hub) {
+		List devices = hub.getAttachedUsbDevices( );
+		Iterator iterator = devices.iterator( );
+		while (iterator.hasNext( )) {
+			UsbDevice device = (UsbDevice) iterator.next( );
+			System.out.println(device);
+			if (device.isUsbHub( )) {
+				listDevices((UsbHub) device);
+			}
+		}
 	}
 
 }
